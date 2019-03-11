2518a36ace0e8b57d83552553887cb394433c3c2
Stefany Raileanu <raileanu_stefany@yahoo.com>
2019-03-11 19:19:35 +0200

    added components, minor edits on front page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BoardComponent } from './board/board.component';
+import { ScoresComponent } from './scores/scores.component';
+import { TutorialComponent } from './tutorial/tutorial.component';
+import { NameComponent } from './name/name.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BoardComponent,
+    ScoresComponent,
+    TutorialComponent,
+    NameComponent
   ],
   imports: [
     BrowserModule,
