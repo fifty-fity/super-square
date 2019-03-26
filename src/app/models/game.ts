99feb8976103cdd1b35566e5d702d289597d7c39
Grula <20150280@student.act.edu>
2019-03-26 12:34:02 +0200

    Previous smidge cleaner

--- src/app/models/game.ts
+++ src/app/models/game.ts
@@ -31,7 +31,7 @@ export class Game {
   }
 
   public endGame() {
-    alert("The game ended");
+    // alert("The game ended");
     this.currentLevel = 1;
     this.sessionPlayer.setScore(0);
     this.sessionPlayer.setLives(3);
@@ -46,17 +46,13 @@ export class Game {
     this.sessionPlayer.loseLife();
   }
 
-  public static updateScore(score: number) {
+  public updateScore(score: number) {
     this.sessionPlayer.addScore(score);
-  }
-  
-  public static test(){
-    alert("test");
   }
 
   public frame(evt: KeyboardEvent) {
     //this works
-    //Game.test();
+    //Game.endGame();
 
     //this doesnt
     //this.updateScore(100);
@@ -70,12 +66,12 @@ export class Game {
         case "ArrowDown":
           // Do something for "down arrow" key press.
           
-          var player: HTMLElement = document.getElementsByClassName('player')[0];
+          var player: Element = document.getElementsByClassName('player')[0];
 
           var player_id = player.parentElement.getAttribute("id");
           
           var below_block_id = 10 + Number(player_id);
-          var below_block = document.getElementById(below_block_id).firstChild;
+          var below_block: Element = document.getElementById(String(below_block_id)).firstChild;
           var below_block_class = below_block.getAttribute("class");
           
           // alert(player_id);
@@ -84,34 +80,20 @@ export class Game {
           //this.test();
 
           while(below_block_class != "wall"){
-            if(below_block_class == "void"){
-
               below_block.setAttribute("class", "player");
               below_block.firstChild.setAttribute("src", "assets/images/player.png");
               player.setAttribute("class", "void");
               player.firstChild.setAttribute("src", "assets/images/void.png");
+
+            if(below_block_class == "void"){
               // alert("done");
             }else if(below_block_class == "coin"){
-              
-              below_block.setAttribute("class", "player");
-              below_block.firstChild.setAttribute("src", "assets/images/player.png");
-              player.setAttribute("class", "void");
-              player.firstChild.setAttribute("src", "assets/images/void.png");
               //this.updateScore(100);
-
             }else if(below_block_class == "bomb"){
               //this.updateLives();
-              below_block.setAttribute("class", "player");
-              below_block.firstChild.setAttribute("src", "assets/images/player.png");
-              player.setAttribute("class", "void");
-              player.firstChild.setAttribute("src", "assets/images/void.png");
 
             }else if(below_block_class == "question"){
               //this.updateScore(1);
-              below_block.setAttribute("class", "player");
-              below_block.firstChild.setAttribute("src", "assets/images/player.png");
-              player.setAttribute("class", "void");
-              player.firstChild.setAttribute("src", "assets/images/void.png");
 
             }else if(below_block_class == "finish"){
               //endGame();
@@ -125,103 +107,73 @@ export class Game {
         case "ArrowUp":
           // Do something for "down arrow" key press.
           
-          var player: HTMLElement = document.getElementsByClassName('player')[0];
+          var player: Element = document.getElementsByClassName('player')[0];
 
           var player_id = player.parentElement.getAttribute("id");
-          var below_block_id = Number(player_id) - 10;
+          var above_block_id = Number(player_id) - 10;
 
-          var below_block = document.getElementById(below_block_id).firstChild;
-          var below_block_class = below_block.getAttribute("class");
+          var above_block: Element = document.getElementById(String(above_block_id)).firstChild;
+          var above_block_class = above_block.getAttribute("class");
           
           // alert(player_id);
-          // alert(below_block_id);
-          // alert(below_block_class);
+          // alert(above_block_id);
+          // alert(above_block_class);
 
-          while(below_block_class != "wall"){
-            if(below_block_class == "void"){
-              below_block.setAttribute("class", "player");
-              below_block.firstChild.setAttribute("src", "assets/images/player.png");
+          while(above_block_class != "wall"){
+              above_block.setAttribute("class", "player");
+              above_block.firstChild.setAttribute("src", "assets/images/player.png");
               player.setAttribute("class", "void");
               player.firstChild.setAttribute("src", "assets/images/void.png");
+            if(above_block_class == "void"){
               // alert("done");
-            }else if(below_block_class == "coin"){
+            }else if(above_block_class == "coin"){
               //this.updateScore(100);
-              below_block.setAttribute("class", "player");
-              below_block.firstChild.setAttribute("src", "assets/images/player.png");
-              player.setAttribute("class", "void");
-              player.firstChild.setAttribute("src", "assets/images/void.png");
-
-            }else if(below_block_class == "bomb"){
+            }else if(above_block_class == "bomb"){
               //this.updateLives();
-              below_block.setAttribute("class", "player");
-              below_block.firstChild.setAttribute("src", "assets/images/player.png");
-              player.setAttribute("class", "void");
-              player.firstChild.setAttribute("src", "assets/images/void.png");
-
-            }else if(below_block_class == "question"){
+            }else if(above_block_class == "question"){
              // this.updateScore(1);
-              below_block.setAttribute("class", "player");
-              below_block.firstChild.setAttribute("src", "assets/images/player.png");
-              player.setAttribute("class", "void");
-              player.firstChild.setAttribute("src", "assets/images/void.png");
-
-            }else if(below_block_class == "finish"){
+            }else if(above_block_class == "finish"){
               //endGame();
             }
-            below_block_id -= 10;
-            below_block = document.getElementById(below_block_id).firstChild;
-            below_block_class = below_block.getAttribute("class");
+            above_block_id -= 10;
+            above_block = document.getElementById(above_block_id).firstChild;
+            above_block_class = above_block.getAttribute("class");
             player = document.getElementsByClassName('player')[0];
          }
           break;
         case "ArrowLeft":
           // Do something for "left arrow" key press.
-          var player: HTMLElement = document.getElementsByClassName('player')[0];
+          var player: Element = document.getElementsByClassName('player')[0];
 
           var player_id = player.parentElement.getAttribute("id");
-          var below_block_id = Number(player_id) -1;
+          var left_block_id = Number(player_id) -1;
 
-          var below_block = document.getElementById(below_block_id).firstChild;
-          var below_block_class = below_block.getAttribute("class");
+          var left_block: Element = document.getElementById(String(left_block_id)).firstChild;
+          var left_block_class = left_block.getAttribute("class");
           
           // alert(player_id);
-          // alert(below_block_id);
-          // alert(below_block_class);
+          // alert(left_block_id);
+          // alert(left_block_class);
 
-          while(below_block_class != "wall"){
-            if(below_block_class == "void"){
-              below_block.setAttribute("class", "player");
-              below_block.firstChild.setAttribute("src", "assets/images/player.png");
+          while(left_block_class != "wall"){
+              left_block.setAttribute("class", "player");
+              left_block.firstChild.setAttribute("src", "assets/images/player.png");
               player.setAttribute("class", "void");
               player.firstChild.setAttribute("src", "assets/images/void.png");
+            if(left_block_class == "void"){
               // alert("done");
-            }else if(below_block_class == "coin"){
+            }else if(left_block_class == "coin"){
              // this.updateScore(100);
-              below_block.setAttribute("class", "player");
-              below_block.firstChild.setAttribute("src", "assets/images/player.png");
-              player.setAttribute("class", "void");
-              player.firstChild.setAttribute("src", "assets/images/void.png");
-
-            }else if(below_block_class == "bomb"){
+            }else if(left_block_class == "bomb"){
              // this.updateLives();
-              below_block.setAttribute("class", "player");
-              below_block.firstChild.setAttribute("src", "assets/images/player.png");
-              player.setAttribute("class", "void");
-              player.firstChild.setAttribute("src", "assets/images/void.png");
-
-            }else if(below_block_class == "question"){
+            }else if(left_block_class == "question"){
             //  this.updateScore(1);
-              below_block.setAttribute("class", "player");
-              below_block.firstChild.setAttribute("src", "assets/images/player.png");
-              player.setAttribute("class", "void");
-              player.firstChild.setAttribute("src", "assets/images/void.png");
-
-            }else if(below_block_class == "finish"){
+            }else if(left_block_class == "finish"){
               //endGame();
             }
-            below_block_id -= 1;
-            below_block = document.getElementById(below_block_id).firstChild;
-            below_block_class = below_block.getAttribute("class");
+            left_block_id -= 1;
+            left_block = document.getElementById(left_block_id).firstChild;
+            left_block_class = left_block.getAttribute("class");
             player = document.getElementsByClassName('player')[0];
          }
           
@@ -229,52 +181,38 @@ export class Game {
         case "ArrowRight":
           // Do something for "right arrow" key press.
           // Do something for "left arrow" key press.
-          var player: HTMLElement = document.getElementsByClassName('player')[0];
+          var player: Element = document.getElementsByClassName('player')[0];
 
           var player_id = player.parentElement.getAttribute("id");
-          var below_block_id = Number(player_id) + 1;
+          var right_block_id = Number(player_id) + 1;
 
-          var below_block = document.getElementById(below_block_id).firstChild;
-          var below_block_class = below_block.getAttribute("class");
+          var right_block: Element = document.getElementById(String(right_block_id)).firstChild;
+          var right_block_class = right_block.getAttribute("class");
           
           // alert(player_id);
-          // alert(below_block_id);
-          // alert(below_block_class);
 
-          while(below_block_class != "wall"){
-            if(below_block_class == "void"){
-              below_block.setAttribute("class", "player");
-              below_block.firstChild.setAttribute("src", "assets/images/player.png");
-              player.setAttribute("class", "void");
-              player.firstChild.setAttribute("src", "assets/images/void.png");
+          // alert(right_block_id);
+          // alert(right_block_class);
+
+          while(right_block_class != "wall"){
+            right_block.setAttribute("class", "player");
+            right_block.firstChild.setAttribute("src", "assets/images/player.png");
+            player.setAttribute("class", "void");
+            player.firstChild.setAttribute("src", "assets/images/void.png");
+            if(right_block_class == "void"){
               // alert("done");
-            }else if(below_block_class == "coin"){
+            }else if(right_block_class == "coin"){
             //  this.updateScore(100);
-              below_block.setAttribute("class", "player");
-              below_block.firstChild.setAttribute("src", "assets/images/player.png");
-              player.setAttribute("class", "void");
-              player.firstChild.setAttribute("src", "assets/images/void.png");
-
-            }else if(below_block_class == "bomb"){
+            }else if(right_block_class == "bomb"){
            //   this.updateLives();
-              below_block.setAttribute("class", "player");
-              below_block.firstChild.setAttribute("src", "assets/images/player.png");
-              player.setAttribute("class", "void");
-              player.firstChild.setAttribute("src", "assets/images/void.png");
-
-            }else if(below_block_class == "question"){
+            }else if(right_block_class == "question"){
             //  this.updateScore(1);
-              below_block.setAttribute("class", "player");
-              below_block.firstChild.setAttribute("src", "assets/images/player.png");
-              player.setAttribute("class", "void");
-              player.firstChild.setAttribute("src", "assets/images/void.png");
-
-            }else if(below_block_class == "finish"){
+            }else if(right_block_class == "finish"){
               //endGame();
             }
-            below_block_id += 1;
-            below_block = document.getElementById(below_block_id).firstChild;
-            below_block_class = below_block.getAttribute("class");
+            right_block_id += 1;
+            right_block = document.getElementById(right_block_id).firstChild;
+            right_block_class = right_block.getAttribute("class");
             player = document.getElementsByClassName('player')[0];
          }
           break;
@@ -287,6 +225,4 @@ export class Game {
       event.preventDefault();
     }, true);
   }
-
-
 }
